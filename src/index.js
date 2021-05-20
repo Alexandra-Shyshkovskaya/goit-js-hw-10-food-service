0328fcf9f39261c233c57f4e509bb4ce06e507ea
Alexandra-Shyshkovskaya <74681434+Alexandra-Shyshkovskaya@users.noreply.github.com>
2021-05-20 06:36:32 +0300

    work with scss

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
+import menuCardsTpl from './templates/menu.hbs';
+import menu from './menu.json';
 import './sass/main.scss';
+
+const menuRef = document.querySelector('.js-menu');
+const menuMarkup = menuCardsTpl(menu);
+menuRef.insertAdjacentHTML('beforeend', menuMarkup);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const THEME_KEY = 'theme';
+
+const themeRef = document.querySelector('#theme-switch-toggle');
+
+themeRef.addEventListener('change', onThemeChange);
+
+function onThemeChange() {
+  replaceTheme(Theme.LIGHT, Theme.DARK);
+
+  localStorage.setItem(THEME_KEY, document.body.classList.value);
+}
+
+function replaceTheme(oldTheme, newTheme) {
+  document.body.classList.add(oldTheme);
+
+  themeRef.checked
+    ? document.body.classList.replace(oldTheme, newTheme)
+    : document.body.classList.replace(newTheme, oldTheme);
+}
+
+function savedTheme() {
+  const savedTheme = localStorage.getItem(THEME_KEY);
+  if (savedTheme) {
+    document.body.classList.value = savedTheme;
+
+    themeRef.checked = savedTheme === Theme.DARK;
+
+
+  }
+}
+
+savedTheme();
